dfa55d12fa9a4d8d5c514de1e25629017d01de7b
chaen243 <codms1357@naver.com>
2024-03-26 12:07:01 +0900

    class

--- tf114/tf06_linear5_placeholder.py
+++ tf114/tf06_linear5_placeholder.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 tf.compat.v1.set_random_seed(777)
 
 #1. 데이터
-x = [1,2,3,4,5]
-y = [3,5,7,9,11]
+x = tf.compat.v1.placeholder(tf.float32, shape=[None]) #placeholder로 지정
+y = tf.compat.v1.placeholder(tf.float32, shape=[None]) #placeholder로 지정
 
 
 # w = tf.Variable(111, dtype=tf.float32) #weight
@@ -13,9 +13,6 @@ y = [3,5,7,9,11]
 w = tf.Variable(tf.random_normal([1]), dtype=tf.float32) #정규분포
 b = tf.Variable(tf.random_normal([1]), dtype=tf.float32) #정규분포
 
-sess = tf.compat.v1.Session()
-sess.run(tf.global_variables_initializer())
-print(sess.run(w))
 
 
 #2. 모델구성
@@ -40,11 +37,18 @@ train = optimizer.minimize(loss) #(로스를 줄인다)
 #with가 알아서 session을 close 해줌!
 with tf.compat.v1.Session() as sess:
     sess.run(tf.global_variables_initializer()) #위의 변수를 초기화!
+    X = [1, 2, 3, 4, 5]
+    Y = [3, 5, 7, 9, 11]
+    #변수로 미리 지정해주기 위해서는  for문 안의 변수와 다른 이름으로 지정해줘야함
 
+    
     #model.fit
     epochs= 2000 #for문!
     for step in range(epochs):
-        sess.run(train)
+        y_predict, w_val, b_val, _ = sess.run([loss, w, b, train],
+        #feed_dict = {x: [1,2,3,4,5], y : [3,5,7,9,11] }) #값지정해줌
+        feed_dict = {x: X, y : Y }) #값지정해줌
+        
         if step % 20 == 0: #tf2의 verbose
-            print(step+1, sess.run(loss), sess.run(w), sess.run(b)) #verbose와 model.weight에서 봤던것들
+            print(step+1, y_predict, w_val, b_val) #verbose와 model.weight에서 봤던것들
 #    sess.close()
